afa70beedb5fb341c9f50e49c533a13df4c15516
jmccauley2 <jmccauley2@luc.edu>
2016-04-18 14:41:11 -0500

    Added files via upload
    Skeleton created.  Functions declared.

---
+++ HW5.cpp
@@ -0,0 +1,53 @@
+// HW5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+void Bubble(string file);
+void Insertion(string file);
+void Quick(string file);
+void Shell(string file);
+void Merge(string file);
+
+int main()
+{
+	string files [5] = { "Bubble","Insertion","Quick","Shell","Merge" };
+	for each (string file in files)
+	{
+		Bubble(file);
+		Insertion(file);
+		Quick(file);
+		Shell(file);
+		Merge(file);
+	}
+    return 0;
+}
+
+void Bubble(string file)
+{
+
+}
+
+void Insertion(string file)
+{
+
+}
+
+void Quick(string file)
+{
+
+}
+
+void Shell(string file)
+{
+
+}
+
+void Merge(string file)
+{
+
+}
